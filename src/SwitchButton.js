3fa19bc6c9024cc71ea537be2cfd721224ef893c
Marco <marcogarzonlara@hotmail.com>
2020-12-09 14:22:40 +0800

    Separated old and new app into their own respective buttons.

--- src/SwitchButton.js
+++ src/SwitchButton.js
@@ -3,8 +3,11 @@ import {Button} from 'antd'
 import{createElement as h} from 'react'
 
 export function SwitchButton({setState}){
-    return h(Button, {shape: 'round', onClick(){
-        // main()
-        setState({subApp: 'NewApp'})
-    }}, 'Show new app')
+    return h('div', {id: 'appbuttons'}, 
+        h(Button, {shape: 'round', onClick(){
+            setState({subApp: 'NewApp'})
+        }}, 'show new app'),
+        h(Button, {shape: 'round', onClick(){
+            setState({subApp: 'LegApp'})
+        }}, 'show old app'))
 }
