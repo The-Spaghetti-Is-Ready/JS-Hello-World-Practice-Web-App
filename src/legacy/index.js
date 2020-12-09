7fd767bf309919f6ca6b82a0e70054f14bd02120
Marco <marcogarzonlara@hotmail.com>
2020-12-09 14:53:33 +0800

    Improved app version button functionality.

--- src/legacy/index.js
+++ src/legacy/index.js
@@ -3,11 +3,12 @@ import {main} from './helloworld'
 import {Button} from 'antd'
 
 export function Legacy(){
+  useEffect(() => {
+    main()
+  }, [])
+  
   return h('div',{id: 'root2'},
     h('h1', {id: 'hello', style: {color: 'orange'}}),
     h(Button, {id: 'button', shape: 'round', onClick(){}}, 'change'),
-    useEffect(() => {
-      main()
-    }, [])
   ) 
 }
