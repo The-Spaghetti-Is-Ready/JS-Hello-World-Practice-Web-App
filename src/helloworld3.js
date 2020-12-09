29cbce76ee4a2a4d0f91b089b90a219f7a1e8948
Marco <marcogarzonlara@hotmail.com>
2020-12-09 11:28:05 +0800

    (UNSTABLE BUILD)Changed legacy button so that it is no longer legacy HTML. It is now a dynamic button.

--- src/helloworld3.js
+++ src/helloworld3.js
@@ -1,17 +1,25 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import {TestDiv} from './react app/TestDiv'
-import {SwitchButton} from './react app/SwitchButton';
+import {SwitchButton} from './react app/SwitchButton'
+import {Button} from 'antd'
 const h = React.createElement
 
 export function mainThree(){
   ReactDOM.render(
     h('div', {},
       h(SwitchButton),
-      h(TestDiv)
+      h(TestDiv),
+      h('div',{id: 'root2'},
+        h('h1', {id: 'hello', style: {color: 'orange'}}),
+        h(Button, {id: 'button', shape: 'round', onclick(){}}, 'change')
+      )
     ),
     document.getElementById('root')
   )
 }  
 
-  
+/* <div id='root2'>
+<h1 id="hello" style = "color: orange;"></h1>
+<button id ="button">change</button>
+</div> */
