d8f9fb5341ff5b2541b45f2aaa93c9c20b33b3de
Marco <marcogarzonlara@hotmail.com>
2021-01-06 14:24:39 +0800

    Create report button shows github account information

--- src/RentalsReport/RentalsReport.js
+++ src/RentalsReport/RentalsReport.js
@@ -1,8 +1,12 @@
+import React from 'react'
 import { Button } from 'antd'
 import { createElement as h} from 'react'
+import axios from 'axios'
 
 export function RentalsReport(){
-  return h(Button, {shape: 'round', onClick(){
+  return h(Button, {shape: 'round', async onClick(){
+    const response = await axios.get('https://api.github.com/users/The-Spaghetti-Is-Ready')
+    console.log('User info: ', response.data)
     console.debug("button pressed")
   }},'Create Report')
 }
